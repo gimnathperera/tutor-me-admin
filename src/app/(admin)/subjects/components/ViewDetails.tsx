e0a41f9616fe3dd6925608a7599a0a0422a95c4e
Jayathu Shehan Dewmina <jayathushehan@gmail.com>
2025-08-26 15:58:16 +0530

    fix: z indexes

--- src/app/(admin)/subjects/components/ViewDetails.tsx
+++ src/app/(admin)/subjects/components/ViewDetails.tsx
@@ -22,7 +22,7 @@ export function SubjectDetails({ title, description }: SubjectDetailsProps) {
       <DialogTrigger asChild>
         <Eye cursor="pointer" />
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px] bg-white z-[9999] dark:bg-gray-800 dark:text-white/90">
+      <DialogContent className="sm:max-w-[425px] bg-white z-50 dark:bg-gray-800 dark:text-white/90">
         <DialogHeader>
           <DialogTitle>Details</DialogTitle>
           <DialogDescription>
